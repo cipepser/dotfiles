96ba1e79385557e0c01bd4b650ff6f95b785de3a
cipepser <respepic@gmail.com>
2026-07-18 14:48:21 +0900

    use peco in nixos too

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -17,7 +17,7 @@ setopt hist_ignore_space # ignore when commands starts with space
 setopt share_history     # share command history data
 
 function peco_select_history() {
-  BUFFER=`history -n 1 | tail -r  | awk '!a[$0]++' | peco`
+  BUFFER=`history -rn 1 | awk '!a[$0]++' | peco`
   CURSOR=$#BUFFER
   zle reset-prompt
 }
